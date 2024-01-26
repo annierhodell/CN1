008571bdf29af080fdb15d4b6c4a11d1582d5c3f
Annie O'Dell <123157819+annierhodell@users.noreply.github.com>
2024-01-26 15:57:48 +0000

    end of ws1

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-name = 'Annie'
-age = 20
-birthday = '7th of October'
-print(f'On the {birthday} I will be {age + 1} years old')
+name_1 = 'Annie'
+age_1 = 20
+birthday_1 = 'October 7th'
+name_2 = 'Sam'
+age_2 = 21
+birthday_2 = 'October 6th'
+print(f'{name_1} was {age_1} on {birthday_1}, while {name_2} was {age_2} on {birthday_2}.')
+print(f'is {name_1} older than {name_2}?' <True/False>)
