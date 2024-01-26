113a721ff410c7653b81867deab7a665082d4148
Annie O'Dell <123157819+annierhodell@users.noreply.github.com>
2024-01-26 15:47:15 +0000

    added birthday message to hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-name = 'Josephine'
-age = 36
-
+name = 'Annie'
+age = 20
+birthday = '7th of October'
 print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f'My birthday is on the {birthday}')
