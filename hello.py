ce314b693673aad68de945cd3e8ee5253a64b52a
Annie O'Dell <123157819+annierhodell@users.noreply.github.com>
2024-01-26 15:52:14 +0000

    birthday aging

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 name = 'Annie'
 age = 20
 birthday = '7th of October'
-print(f'Hi, my name is {name}, and I am {age} years old.')
-print(f'My birthday is on the {birthday}')
+print(f'On the {birthday} I will be {age + 1} years old')
